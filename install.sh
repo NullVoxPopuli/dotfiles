f69c4e21fa0627d5ada5e71bac9c1ed40c049459
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-08-21 08:40:10 -0400

    add gnome config and install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+###############################################
+# Load Ubuntu/Gnome Configuration + KeyMappings
+###############################################
+cat home/.config/dconf/user.d/* | dconf load /
+
+# NOTE: to export dconf settings:
+# dconf dump / > ~/.config/dconf/user.d/everything.conf
+
+###########################
+# Copy home directory stuff
+###########################
+cp home/.atom ~/ -R
+cp home/.i3 ~/ -R
+cp home/.vim ~/ -R
+cp home/scripts ~/ -R
+cp home/.bash_profile ~/
+cp home/.tmux.conf ~/
+cp home/.vimrc ~/
