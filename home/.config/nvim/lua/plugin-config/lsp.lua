773d5769815bb369f1a2f6ea669c359deed6e1ce
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-05-02 23:30:18 -0400

    Customize appearance of completion, keep border around docs -- switch to c-j and c-k for docs scrolling

--- home/.config/nvim/lua/plugin-config/lsp.lua
+++ home/.config/nvim/lua/plugin-config/lsp.lua
@@ -31,13 +31,13 @@ cmp.setup({
     end,
   },
   window = {
-    completion = cmp.config.window.bordered(),
+    -- completion = cmp.config.window.bordered(),
     documentation = cmp.config.window.bordered(),
   },
   mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-k>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-j>'] = cmp.mapping.scroll_docs(4),
+    -- ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     ['<Tab>'] = function(fallback)
