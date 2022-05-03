46d8e664e72a0c1de7891657ddc981adf144a078
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-05-02 23:57:18 -0400

    Two fewer completion sources

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -4,8 +4,6 @@ return function(use)
   use 'williamboman/nvim-lsp-installer'
   use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
   use 'hrsh7th/nvim-cmp'
   use 'L3MON4D3/LuaSnip'
 end
