5a9c08e17a0ff5722afe1ce8909ca617c8f39eb1
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-05-05 13:56:11 -0400

    Glint support lands in lsp-configs by default

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -1,7 +1,7 @@
 return function(use)
-  -- use 'neovim/nvim-lspconfig'
+  use 'neovim/nvim-lspconfig'
   -- use '~/Development/NullVoxPopuli/nvim-lspconfig'
-  use { 'muziejus/nvim-lspconfig', branch = "glint" }
+  -- use { 'muziejus/nvim-lspconfig', branch = "glint" }
   use 'williamboman/nvim-lsp-installer'
   use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/cmp-buffer'
