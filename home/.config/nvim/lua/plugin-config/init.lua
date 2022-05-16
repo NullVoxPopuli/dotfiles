0174da4daae2027c47930ca13b3b76d3ebbc72e9
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-05-16 16:01:51 -0400

    Re-enable nvim-notify

--- home/.config/nvim/lua/plugin-config/init.lua
+++ home/.config/nvim/lua/plugin-config/init.lua
@@ -3,9 +3,8 @@ vim.api.nvim_command [[set termguicolors]]
 
 require('plugin-config.lsp')
 
--- setting default level appears broken?
--- See: https://github.com/rcarriga/nvim-notify/issues/77#issuecomment-1127091726
--- vim.notify = require('notify')
--- vim.notify.setup { level = 'error' }
+-- Fancy notification from nvim-notify
+vim.notify = require('notify')
+vim.notify.setup { level = 'warn' }
 
 
