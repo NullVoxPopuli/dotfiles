d1776eb447753d6dbd01f7e8ab6e87e9e4a43660
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-02-21 14:33:42 -0500

    Updates

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # Install Common Deps
 #####################
 # quality of life
-sudo apt install xclip gufw exa bat libappindicator1 fonts-font-awesome fonts-powerline
+sudo apt install xclip gufw exa bat libappindicator1 fonts-font-awesome fonts-powerline 
 
 # personal stuff for various projects
 sudo apt install gparted audacity
@@ -13,9 +13,10 @@ sudo apt install gparted audacity
 #   gimp
 
 # hobby / work stuff
-sudo apt install git curl build-essential i3 python3-pip imagemagick
+sudo apt install git curl build-essential i3 python3-pip imagemagick fzf
 
 sudo snap install dust
+sudo snap install starship
 
 # Volta / node manager
 curl https://get.volta.sh | bash
@@ -29,6 +30,11 @@ pip3 install --user pynvim
 # packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+mkdir -p ~/Development/NullVoxPopuli
+mkdir -p ~/Development/OpenSource
+mkdir -p ~/Development/tmp
+mkdir -p ~/Development/Work
 
 mkdir -p ~/.local/share/nvim/_backup
 mkdir -p ~/.local/share/nvim/_temp
