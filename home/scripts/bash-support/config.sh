a7d2ffd43d72d152237c977d4ade1e2120b349f6
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-05-10 17:34:37 -0400

    Support entering the GPG Key password/phrase in the terminal

--- home/scripts/bash-support/config.sh
+++ home/scripts/bash-support/config.sh
@@ -1,9 +1,16 @@
-export EDITOR=nvim
 export PROMPT_COMMAND='echo -ne "\033]0;$(basename ${PWD})\007"'
 export NODE_OPTIONS='--trace-warnings --unhandled-rejections=strict'
+
+# Navigating repos
 export DEV="$HOME/Development"
 export OSS="$DEV/OpenSource"
 export GG_PATHS="$DEV/NullVoxPopuli:$DEV/tmp:$DEV/Work:$OSS:$OSS/emberjs:$OSS/starbeam"
 export GG_PREFIX="$HOME/Development/"
+
+# Editor
+export EDITOR=nvim
 export COPILOT="true"
 export PRETTIERD_LOCAL_PRETTIER_ONLY=1
+
+# Sehcuurrity 
+export GPG_TTY=$(tty)
