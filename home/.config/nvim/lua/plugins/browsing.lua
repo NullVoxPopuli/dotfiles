4c41ba3e1afd7a8a81fce53b412ba563ef69b939
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-09-08 10:36:50 -0400

    window picker

--- home/.config/nvim/lua/plugins/browsing.lua
+++ home/.config/nvim/lua/plugins/browsing.lua
@@ -18,12 +18,32 @@ return function(use)
   vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
   use {
+      's1n7ax/nvim-window-picker',
+      tag = 'v2.*',
+      config = function()
+          local colors = {
+            fg = '#ffffff',
+            bg = '#5D3FD3',
+            bold = true
+          }
+          require'window-picker'.setup({
+            selection_chars = dvorak_keys,
+            highlights = {
+              statusline = {
+                unfocused = colors
+              }
+            }
+          })
+      end,
+  }
+  use {
     "nvim-neo-tree/neo-tree.nvim",
       branch = "v3.x",
       requires = { 
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
         "MunifTanjim/nui.nvim",
+        "s1n7ax/nvim-window-picker"
       },
       config = function () 
         require('neo-tree').setup({
@@ -39,6 +59,11 @@ return function(use)
           buffers = { 
             follow_current_file = true
           },
+          window = {
+            mappings = {
+              ["<cr>"] = "open_with_window_picker"
+            }
+          }
         })
       end
     }
