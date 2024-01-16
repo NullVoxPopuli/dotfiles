b595497091e35976e1dd2df6c53caece9b36a607
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-01-16 15:53:27 -0500

    Update keymaps

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -150,8 +150,7 @@ require('packer').startup(function(use)
         bottom_char = "─"
       }
     })
-    -- vim.keymap.set('n', 'gd', '<CMD>Glance declaration<CR>')
-    -- vim.keymap.set('n', 'gD', '<CMD>Glance definition<CR>')
+    vim.keymap.set('n', 'gd', '<CMD>Glance definitions<CR>')
     vim.keymap.set('n', 'gi', '<CMD>Glance implementations<CR>')
     vim.keymap.set('n', 'gt', '<CMD>Glance type_definitions<CR>')
     vim.keymap.set('n', 'gr', '<CMD>Glance references<CR>')
