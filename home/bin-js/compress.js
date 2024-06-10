21df3d58d1e51f4b1b63bbd9667cf7e582d744d7
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-06-10 18:25:09 -0400

    Adjust settings

--- home/bin-js/compress.js
+++ home/bin-js/compress.js
@@ -4,11 +4,29 @@ import { filesize } from 'filesize';
 import { minify } from 'terser';
 import { execa,  execaCommand, $ } from 'execa';
 
+const [, , subPath] = process.argv;
+
 const options =  {
   parse: {
     html5_comments: false,
   },
-  compress: true,
+  compress: {
+    ecma: 2022,
+    passes: 6,
+    toplevel: false,
+    defaults: true,
+    arguments: false,
+    keep_fargs: false,
+    toplevel: false,
+    // Danger!
+    unsafe: true,
+    unsafe_comps: false,
+    unsafe_math: true,
+    unsafe_symbols: true,
+    unsafe_proto: true,
+    unsafe_undefined: true,
+  },
+  toplevel: false,
   output: {
     ecma: 2022,
     comments: false,
@@ -20,7 +38,7 @@ const getSize = (file) => {
   return filesize(size)
 }
 
-const files = fs.globSync(path.join(process.cwd(), '/**/*.js'))
+const files = fs.globSync(path.join(process.cwd(), subPath || '.', '/**/*.js'))
 
 for (let file of files) {
   console.log(`Minifying ${file} (${getSize(file)})`)
@@ -41,10 +59,13 @@ for (let file of files) {
 }
 
 /**
+ * Run via:
+ * ❯ node ~/Development/NullVoxPopuli/dotfiles/home/bin-js/compress.js ./dist/ 
+ *
  * See sizes via:
- * ❯ du --depth 1 --reverse --apparent-size --no-percent-bars --filter ".js$" ember-composable-helpers/dist/
- * ❯ du --depth 1 --reverse --apparent-size --no-percent-bars --filter ".min$" ember-composable-helpers/dist/
- * ❯ du --depth 1 --reverse --apparent-size --no-percent-bars --filter ".gz$" ember-composable-helpers/dist/
- * ❯ du --depth 1 --reverse --apparent-size --no-percent-bars --filter ".br$" ember-composable-helpers/dist/
+ * ❯ du --depth 1 --reverse --apparent-size --no-percent-bars --filter ".js$" ./dist/
+ * ❯ du --depth 1 --reverse --apparent-size --no-percent-bars --filter ".min$" ./dist/
+ * ❯ du --depth 1 --reverse --apparent-size --no-percent-bars --filter ".gz$" ./dist/
+ * ❯ du --depth 1 --reverse --apparent-size --no-percent-bars --filter ".br$" ./dist/
  *
  */
