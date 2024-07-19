15edf15515c1deb7e2df8e61daaebb8f4b87d503
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-07-19 14:17:39 -0400

    Update mac-related tweaks

--- os/mac/setup.sh
+++ os/mac/setup.sh
@@ -16,6 +16,7 @@ ln -s $PWD/os/mac/home/.config/skhd/skhdrc ~/.config/skhd/skhdrc
 rm -f ~/.config/alacritty; ln -s $PWD/os/mac/home/.config/alacritty ~/.config/alacritty
 rm -f ~/.bashrc; ln -s $PWD/os/mac/home/.bashrc ~/.bashrc
 
+rm -fr ~/.config/alacritty/themes
 git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
 
 # NOTE: karabiner conflicts with skhd *and* breaks the commands that it overrides
@@ -23,10 +24,11 @@ git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/theme
 #       Do not use karabiner.
 # 
 # Fix home and end -- How do you do this sytem wide?
-bindkey '\e[H'  beginning-of-line
-bindkey '\eOH'  beginning-of-line
-bindkey '\e[F'  end-of-line
-bindkey '\eOF'  end-of-line
+# NOTE: bindkey is for zsh, bind for bash
+bind '\e[H'  beginning-of-line
+bind '\eOH'  beginning-of-line
+bind '\e[F'  end-of-line
+bind '\eOF'  end-of-line
 
 # Show all files, hidden, etc
 # good for .dotfiles
