57ac7d79fea6606e792791f2ba520c2905d837e4
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-09-03 18:05:59 -0400

    Ember monorepo stats script

---
+++ home/bin-js/ember-monorepo-stats.js
@@ -0,0 +1,69 @@
+import { ember, project } from "ember-apply";
+import { inspect } from "node:util";
+
+let stats = {
+  projects: {
+    ember: {
+      app: {
+        "test-app": 0,
+        "real-app": 0,
+        get total() {
+          return this["test-app"] + this["real-app"];
+        },
+      },
+      library: {
+        v2: 0,
+        v1: 0,
+        get total() {
+          return this.v1 + this.v2;
+        },
+      },
+      get total() {
+        return this.library.total + this.app.total;
+      },
+    },
+    other: 0,
+    get total() {
+      return this.ember.total + this.other;
+    },
+  },
+};
+
+for await (let workspace of await project.eachWorkspace()) {
+  let isEmber = await ember.isEmberProject();
+
+  if (!isEmber) {
+    stats.projects.other++;
+    continue;
+  }
+
+  let isApp = await ember.isApp();
+
+  if (isApp) {
+    if (workspace.includes("/test-app")) {
+      stats.projects.ember.app["test-app"]++;
+      continue;
+    }
+    stats.projects.ember.app["real-app"]++;
+    continue;
+  }
+
+  let isV2 = await ember.isV2Addon();
+
+  if (isV2) {
+    stats.projects.ember.library.v2++;
+    continue;
+  }
+  stats.projects.ember.library.v1++;
+}
+
+console.log(
+  inspect(stats, {
+    depth: Infinity,
+    colors: true,
+    showProxy: true,
+    showHidden: true,
+    getters: true,
+    numericSeparator: true,
+  }),
+);
