c42edbb0a4baf3d164f0d14f1be5463b343f91af
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-10-17 13:15:21 -0400

    Progerss

--- home/.config/nvim/__tests/ember/highlighting/config.lua
+++ home/.config/nvim/__tests/ember/highlighting/config.lua
@@ -1,38 +1,57 @@
+-- This example dosen't use a package manager.
+-- Exact details for how to get plugins will depend on your package manager, 
+--   if you're using one.
+
 vim.api.nvim_command [[set termguicolors]]
 vim.api.nvim_command [[syntax on]]
 
 local now = vim.fn.system({ 'date', '+%s' })
-local tmp_folder = vim.fn.system({ 'mktemp' , '-t', 'nvim-test-' .. now })
+local tmp_folder = os.tmpname() .. '/nvim-test-' .. now
+
+-- remove the trailing new line
+tmp_folder = vim.fn.trim(tmp_folder)
+
 vim.fn.system({ 'mkdir', '-p', tmp_folder })
 
-vim.print("Plugins will be stored in :: " .. tmp_folder)
+vim.print("Plugins will be stored in :: '" .. tmp_folder .. "'")
 
+function install(repo, name)
+  vim.print("Installing " .. name)
+  vim.fn.system({ 'git', 'clone', repo,  tmp_folder .. '/' .. name })
 
-vim.fn.system({ 'git', 'clone', 'https://github.com/nvim-treesitter/nvim-treesitter.git',  tmp_folder .. '/nvim-treesitter' })
+  -- Add plugins to runtime path so that require can find them
+  vim.opt.rtp:append(tmp_folder .. "/" .. name)
+end
 
--- Add plugins to runtime path so that require can find them
-vim.opt.rtp:append(tmp_folder .. "/nvim-treesitter")
+install('https://github.com/nvim-treesitter/nvim-treesitter.git', 'nvim-treesitter')
 
-require("nvim-treesitter.install").prefer_git = true
-require 'nvim-treesitter.configs'.setup {
-  sync_install = true,
-  ignore_install = {},
-  auto_install = true,
-  modules = {},
-  ensure_installed = {
-    "javascript", "typescript",
-    "glimmer", "glimmer_javascript", "glimmer_typescript",
-    "jsdoc",
-    "diff",
-  },
-  highlight = {
-    enable = true,
-  },
-  indent = {
-    enable = true
-  },
-}
+vim.api.nvim_create_autocmd('BufRead', {
+  pattern = { "*.gjs", "*.gts" },
 
-local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+  callback = function()
+    require("nvim-treesitter.install").prefer_git = true
+    require 'nvim-treesitter.configs'.setup {
+      sync_install = true,
+      ignore_install = {},
+      auto_install = true,
+      modules = {},
+      ensure_installed = {
+        "javascript", "typescript",
+        "glimmer", "glimmer_javascript", "glimmer_typescript",
+        "jsdoc",
+        "diff",
+      },
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true
+      },
+    }
 
-ts_update()
+    local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+
+    ts_update()
+  end
+})
+
