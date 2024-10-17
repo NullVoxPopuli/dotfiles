00f3f2c26b482072a6f9c03cf2330df92ad85e57
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-10-17 12:04:24 -0400

    Difficult

--- home/.config/nvim/__tests/ember/highlighting/config.lua
+++ home/.config/nvim/__tests/ember/highlighting/config.lua
@@ -1,57 +1,36 @@
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
-    install_path })
-  print("Installing packer close and reopen Neovim...")
-end
-
-
-require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function()
-      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-      ts_update()
-    end,
-    config = function()
-      require("nvim-treesitter.install").prefer_git = true
-      require 'nvim-treesitter.configs'.setup {
-        sync_install = true,
-        ignore_install = {},
-        auto_install = true,
-        modules = {},
-        ensure_installed = {
-          "javascript", "typescript",
-          "glimmer", "glimmer_javascript", "glimmer_typescript",
-          "jsdoc",
-          "diff",
-        },
-        highlight = {
-          enable = true,
-        },
-        indent = {
-          enable = true
-        },
-      }
-
-      -- vim.treesitter.language.register('gjs', 'javascript.glimmer')
-      -- vim.treesitter.language.register('gjs', 'typescript.glimmer')
-      -- temporarily
-      -- autocmd BufRead *.gjs set filetype=glimmer_javascript
-      -- autocmd BufRead *.gts set filetype=glimmer_typescript
-    end
-  }
-
-  if PACKER_BOOTSTRAP then
-    require('packer').sync()
-  end
-end)
-
 vim.api.nvim_command [[set termguicolors]]
 vim.api.nvim_command [[syntax on]]
+
+
+local tmp_folder = vim.api.nvim_command(':system mktemp')
+vim.api.nvim_echo({"Cloning plugins to " .. tmp_folder}, false, {})
+
+
+vim.api.nvim_command('!git clone https://github.com/nvim-treesitter/nvim-treesitter.git')
+
+-- Add plugins to runtime path so that require can find them
+vim.opt.rtp:append(tmp_folder .. "/nvim-treesitter")
+
+require("nvim-treesitter.install").prefer_git = true
+require 'nvim-treesitter.configs'.setup {
+  sync_install = true,
+  ignore_install = {},
+  auto_install = true,
+  modules = {},
+  ensure_installed = {
+    "javascript", "typescript",
+    "glimmer", "glimmer_javascript", "glimmer_typescript",
+    "jsdoc",
+    "diff",
+  },
+  highlight = {
+    enable = true,
+  },
+  indent = {
+    enable = true
+  },
+}
+
+local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+
+ts_update()
---
+++ home/.config/nvim/__tests/ember/highlighting/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+nvim --clean -u ./config.lua
