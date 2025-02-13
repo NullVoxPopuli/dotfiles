562fe9004d6ce346214b99a632c47f7824922d4b
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-02-13 12:36:09 -0500

    Update create-ram-disk

--- os/ubuntu/create-ram-disk.sh
+++ os/ubuntu/create-ram-disk.sh
@@ -13,9 +13,19 @@
 sudo mkdir -p /mnt/ramdisk
 sudo mount -o size=8G -t tmpfs none /mnt/ramdisk
 
-# To unmount: 
-# sudo umount /mnt/ramdisk
+echo "To unmount:" 
+echo -e "\tsudo umount /mnt/ramdisk"
+echo ""
+echo "Previous pnpm store:"
+echo -e "\t$(pnpm config get store-dir)"
+echo ""
+echo -e "\tTo restore:"
+echo -e "\t\tpnpm config set --global store-dir $(pnpm config get store-dir)"
 
 # TODO: copy store into ramdisk?
 # TODO: download more RAM (store is 9.5G rn in ~/.pnpm-store)
-# pnpm config set store-dir /mnt/ramdisk 
+pnpm config set store-dir /mnt/ramdisk/.pnpm-store 
+
+echo ""
+echo "New pnpm store at:"
+echo "\t$(pnpm config get store-dir)
