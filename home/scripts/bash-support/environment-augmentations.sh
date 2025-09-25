1fe486efc1da16b4826f428743ecd2fc16e70968
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-09-25 18:11:01 -0400

    Configure adb

--- home/scripts/bash-support/environment-augmentations.sh
+++ home/scripts/bash-support/environment-augmentations.sh
@@ -104,3 +104,12 @@ if [ -d "/usr/local/cuda" ]; then
   export PATH="/usr/local/cuda/bin:$PATH" 
   export LD_LIBRARY_PATH="/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
 fi
+
+# Android ADB
+#
+# Following instructions: https://wiki.lineageos.org/adb_fastboot_guide#installing-adb-and-fastboot
+# Using: https://dl.google.com/android/repository/platform-tools-latest-linux.zip
+if [ -d "$HOME/Applications/adb-platform-tools" ] ; then
+ export PATH="$HOME/Applications/adb-platform-tools:$PATH"
+fi
+
