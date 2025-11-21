065fa7e8014aa825092b8c86666c77c5b65e0de5
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-11-21 15:26:07 -0500

    ope

--- home/.config/nvim/lua/plugins/visuals.lua
+++ home/.config/nvim/lua/plugins/visuals.lua
@@ -77,14 +77,17 @@ return function(use)
         particles_enabled = true,
         time_interval = 7, -- milliseconds
 
-        stiffness = 0.5,
-        trailing_stiffness = 0.2,
-        trailing_exponent = 5,
-        damping = 0.6,
+        cursor_color = "#ffffff",
         gradient_exponent = 0,
-        gamma = 1,
-        never_draw_over_target = true, -- if you want to actually see under the cursor
-        hide_target_hack = true,       -- same
+        particle_spread = 1,
+        particles_per_second = 100,
+        particles_per_length = 50,
+        particle_max_lifetime = 1500,
+        particle_max_initial_velocity = 10,
+        particle_velocity_from_cursor = 0,
+        particle_random_velocity = 300,
+        particle_damping = 0.1,
+        particle_gravity = 50,
       })
     end
   }
